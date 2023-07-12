9e1d82473632762b3eb74b90cb349b0fb90db416
o-x-y-g-e-n <shivamgohel777@gmail.com>
2023-07-12 08:17:16 -0700

    gtag working

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,7 @@ module.exports = {
         gtagConfig: {
           anonymize_ip: true,
           cookie_expires: 0,
+          send_page_view: true
         },
         pluginConfig: {
           head: true,
